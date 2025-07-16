4f90ca2a6c468756ad1576fd077a162ce07e2dbe
Mauli Patel <124795820+itsmemauliii@users.noreply.github.com>
2025-07-16 19:25:04 +0530

    Create summarizer.py

---
+++ summarizer.py
@@ -0,0 +1,11 @@
+from transformers import T5Tokenizer, T5ForConditionalGeneration
+
+tokenizer = T5Tokenizer.from_pretrained("t5-small")
+model = T5ForConditionalGeneration.from_pretrained("t5-small")
+
+def summarize_text(text):
+    input_text = "summarize: " + text.strip().replace("\n", " ")
+    inputs = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
+    summary_ids = model.generate(inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
+    output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
+    return output
