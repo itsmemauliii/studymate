e3a836f434283da48b907b7593265b55db3aa2d5
Mauli Patel <124795820+itsmemauliii@users.noreply.github.com>
2025-07-16 21:43:59 +0530

    Update summarizer.py

--- summarizer.py
+++ summarizer.py
@@ -1,15 +1,12 @@
-from transformers import T5Tokenizer, T5ForConditionalGeneration
+from transformers import BartTokenizer, BartForConditionalGeneration
 import torch
 
-tokenizer = T5Tokenizer.from_pretrained("t5-small")
-model = T5ForConditionalGeneration.from_pretrained("t5-small")
+tokenizer = BartTokenizer.from_pretrained("facebook/bart-base")
+model = BartForConditionalGeneration.from_pretrained("facebook/bart-base")
 
 def summarize_text(text):
-    if len(text.split()) > 500:
-        text = " ".join(text.split()[:500])
-
-    input_text = "summarize: " + text.strip().replace("\\n", " ")
-    inputs = tokenizer.encode(input_text, return_tensors="pt", max_length=512, truncation=True)
+    input_text = text.strip().replace("\n", " ")
+    inputs = tokenizer.encode(input_text, return_tensors="pt", max_length=1024, truncation=True)
     summary_ids = model.generate(inputs, max_length=150, min_length=30, length_penalty=2.0, num_beams=4, early_stopping=True)
     output = tokenizer.decode(summary_ids[0], skip_special_tokens=True)
     return output
